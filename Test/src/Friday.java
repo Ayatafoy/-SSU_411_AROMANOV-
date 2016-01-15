f2d3bf2c555ab0a49e1a982cc41fd082dbed736c
unknown <RomanovAI@w12-426-39.main.sgu.ru>
2016-01-15 18:17:04 +0300

    initial_commit

--- Test/src/Friday.java
+++ Test/src/Friday.java
@@ -1,8 +1,10 @@
+import java.io.IOException;
 import java.util.Calendar;
 import java.util.Locale;
+import java.util.Scanner;
 
 public class Friday {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Calendar calendar = Calendar.getInstance();
         System.out.println(calendar.getTime());
         calendar.set(Calendar.MONTH, 0);
@@ -10,5 +12,9 @@ public class Friday {
         System.out.println(calendar.getTime());
         String a = calendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT, Locale.US);
         System.out.println(a);
+        Scanner scanner = new Scanner(System.in);
+        int b = Integer.parseInt(scanner.nextLine());
+        System.out.println(b);
+        System.out.println(b-10);
     }
 }
