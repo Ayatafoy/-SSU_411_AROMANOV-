ae5ac84681571ec895dd24a4a970666bd7363932
unknown <RomanovAI@w12-426-39.main.sgu.ru>
2016-01-15 18:03:34 +0300

    222

--- Test/src/Friday.java
+++ Test/src/Friday.java
@@ -1,3 +1,14 @@
-public class Friday {
+import java.util.Calendar;
+import java.util.Locale;
 
+public class Friday {
+    public static void main(String[] args) {
+        Calendar calendar = Calendar.getInstance();
+        System.out.println(calendar.getTime());
+        calendar.set(Calendar.MONTH, 0);
+        calendar.set(Calendar.DAY_OF_MONTH, 13);
+        System.out.println(calendar.getTime());
+        String a = calendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT, Locale.US);
+        System.out.println(a);
+    }
 }
