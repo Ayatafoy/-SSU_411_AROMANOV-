097276f059815caca5b832e4583f3e73de3e68d0
unknown <teplovalm@w12-416-29.main.sgu.ru>
2016-01-22 17:54:37 +0300

    f

--- Basket/src/Main.java
+++ Basket/src/Main.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main implements Basket{
     ArrayList<String> products = new ArrayList<>();
@@ -41,18 +42,65 @@ public class Main implements Basket{
 
     public static void main(String[] args) {
         Main prod = new Main();
-        prod.addProduct("Системный блок", 1);
-        prod.addProduct("Монитор", 2);
-        prod.addProduct("Мышь", 2);
-        prod.addProduct("Клавиатура", 2);
-        prod.addProduct("Колонки", 2);
-        System.out.println(prod.getProducts());
-        System.out.println(prod.getProductQuantity("Клавиатура"));
-        prod.updateProductQuantity("Клавиатура", 1);
-        prod.removeProduct("Мышь");
-        System.out.println(prod.getProducts());
-        System.out.println(prod.getProductQuantity("Клавиатура"));
-        prod.clear();
-        System.out.println(prod.getProducts());
+        for (; ; ) {
+            System.out.println("Выберите операцию:");
+            System.out.println("Добавить продукт: 1");
+            System.out.println("Удалить продукт: 2");
+            System.out.println("Изменить количество продуктов: 3");
+            System.out.println("Очистить корзину: 4");
+            System.out.println("Вывести список продуктов: 5");
+            System.out.println("Вывести количество: 6");
+            Scanner scanner = new Scanner(System.in);
+            int x = scanner.nextInt();
+
+            switch (x) {
+                case 1: {
+                    System.out.println("Введите название продукта:");
+                    scanner = new Scanner(System.in);
+                    String s = scanner.nextLine();
+                    System.out.println("Введите количество:");
+                    scanner = new Scanner(System.in);
+                    x = scanner.nextInt();
+                    prod.addProduct(s, x);
+                    continue;
+                }
+                case 2: {
+                    System.out.println("Введите название продукта:");
+                    scanner = new Scanner(System.in);
+                    String s = scanner.nextLine();
+                    prod.removeProduct(s);
+                    continue;
+                }
+                case 3: {
+                    System.out.println("Введите название продукта:");
+                    scanner = new Scanner(System.in);
+                    String s = scanner.nextLine();
+                    System.out.println("Введите количество:");
+                    scanner = new Scanner(System.in);
+                    x = scanner.nextInt();
+                    prod.updateProductQuantity(s, x);
+                    continue;
+                }
+                case 4: {
+                    prod.clear();
+                    System.out.println("Корзина очищена!:");
+                    continue;
+                }
+                case 5: {
+                    System.out.println(prod.getProducts());
+                    continue;
+                }
+                case 6: {
+                    System.out.println("Введите название продукта:");
+                    scanner = new Scanner(System.in);
+                    String s = scanner.nextLine();
+                    System.out.println(prod.getProductQuantity(s));
+                    continue;
+                }
+                default:
+                    System.out.println("Operator not found!");
+                    continue;
+            }
+        }
     }
 }
