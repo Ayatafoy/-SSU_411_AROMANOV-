6860409eb195e5ed6919aa1fa029a5eec4393c98
ayatafoy <romanow.lesha2013@yandex.ru>
2016-01-29 17:13:34 +0400

    new_commit

--- CompanyesBuget/src/Main.java
+++ CompanyesBuget/src/Main.java
@@ -1,4 +1,5 @@
 import au.com.bytecode.opencsv.CSVReader;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -7,107 +8,113 @@ import java.util.logging.Logger;
 
 public class Main {
     private static final Logger log = Logger.getLogger("Main");
+
     public static void main(String args[]) throws IOException {
         int k = 0;
         File dir = new File("C:\\Users\\Алексей\\IdeaProjects\\CompanyesBuget\\Resources");
         String fileList[] = dir.list();
         HashMap<String, Pair> map = new HashMap();
         ArrayList<String[]> rows = new ArrayList<>();
-        for (int i = 0; i < fileList.length; i++){
+        for (int i = 0; i < fileList.length; i++) {
             File f = new File(dir + "\\" + fileList[i]);
-            if(!f.isDirectory()) {
+            if (!f.isDirectory()) {
                 CSVReader reader = new CSVReader(new FileReader(f));
-                if (fileList[i].charAt(0) == 'B'){
+                if (fileList[i].charAt(0) == 'B') {
                     List<String[]> row = reader.readAll();
                     reader.close();
-                    String companyName = "", schet = "", buget = "";
+                    String companyName = "";
+                    String checkingAccount = "";
+                    String budget = "";
                     for (int j = 1; j < row.size(); j++) {
-                        while(row.get(j)[0].charAt(k) != ';'){
+                        while (row.get(j)[0].charAt(k) != ';') {
                             companyName += row.get(j)[0].charAt(k);
                             k++;
                         }
                         k++;
-                        while(row.get(j)[0].charAt(k) != ';'){
-                            schet += row.get(j)[0].charAt(k);
+                        while (row.get(j)[0].charAt(k) != ';') {
+                            checkingAccount += row.get(j)[0].charAt(k);
                             k++;
                         }
                         k++;
-                        while(k < row.get(j)[0].length()){
-                            buget += row.get(j)[0].charAt(k);
+                        while (k < row.get(j)[0].length()) {
+                            budget += row.get(j)[0].charAt(k);
                             k++;
                         }
-                        Pair pair = new Pair(companyName, buget);
-                        map.put(schet, pair);
+                        Pair pair = new Pair(companyName, budget);
+                        map.put(checkingAccount, pair);
                         k = 0;
                         companyName = "";
-                        schet = "";
-                        buget = "";
+                        checkingAccount = "";
+                        budget = "";
                     }
-                }
-                else{
-                    if(fileList[i].charAt(0) == 'T'){
+                } else {
+                    if (fileList[i].charAt(0) == 'T') {
                         List<String[]> row = reader.readAll();
                         reader.close();
-                        String companyName1 = "", schetOtpr = "", companyName2 = "", schetPol = "", summa = "";
+                        String companyName1 = "";
+                        String checkingAccountSend = "";
+                        String companyName2 = "";
+                        String checkingAccountRec = "";
+                        String summ = "";
                         for (int j = 1; j < row.size(); j++) {
-                            while(row.get(j)[0].charAt(k) != ';'){
+                            while (row.get(j)[0].charAt(k) != ';') {
                                 companyName1 += row.get(j)[0].charAt(k);
                                 k++;
                             }
                             k++;
-                            while(row.get(j)[0].charAt(k) != ';'){
-                                schetOtpr += row.get(j)[0].charAt(k);
+                            while (row.get(j)[0].charAt(k) != ';') {
+                                checkingAccountSend += row.get(j)[0].charAt(k);
                                 k++;
                             }
                             k++;
-                            while(row.get(j)[0].charAt(k) != ';'){
+                            while (row.get(j)[0].charAt(k) != ';') {
                                 companyName2 += row.get(j)[0].charAt(k);
                                 k++;
                             }
                             k++;
-                            while(row.get(j)[0].charAt(k) != ';'){
-                                schetPol += row.get(j)[0].charAt(k);
+                            while (row.get(j)[0].charAt(k) != ';') {
+                                checkingAccountRec += row.get(j)[0].charAt(k);
                                 k++;
                             }
                             k++;
-                            while(k < row.get(j)[0].length()){
-                                summa += row.get(j)[0].charAt(k);
+                            while (k < row.get(j)[0].length()) {
+                                summ += row.get(j)[0].charAt(k);
                                 k++;
                             }
-                            rows.add(new String[]{companyName1, schetOtpr, companyName2, schetPol, summa});
+                            rows.add(new String[]{companyName1, checkingAccountSend,
+                                    companyName2, checkingAccountRec, summ});
                             k = 0;
                             companyName1 = "";
-                            schetOtpr = "";
+                            checkingAccountSend = "";
                             companyName2 = "";
-                            schetPol = "";
-                            summa = "";
+                            checkingAccountRec = "";
+                            summ = "";
                         }
                     }
                 }
 
-            }
-            else
+            } else
                 continue;
         }
-        for (int i = 0; i < rows.size(); i++){
-            String schet = rows.get(i)[1];
-            String companyName = (String) map.get(schet).first;
-            int budget = Integer.parseInt((String)map.get(schet).second);
+        for (int i = 0; i < rows.size(); i++) {
+            String checkingAccount = rows.get(i)[1];
+            String companyName = (String) map.get(checkingAccount).first;
+            int budget = Integer.parseInt((String) map.get(checkingAccount).second);
             int ostatok = budget - Integer.parseInt(rows.get(i)[4]);
             if (ostatok >= 0) {
-                map.replace(schet, new Pair(companyName, Integer.toString(ostatok)));
-                schet = rows.get(i)[3];
-                companyName = (String) map.get(schet).first;
-                budget = Integer.parseInt((String) map.get(schet).second);
-                map.replace(schet, new Pair(companyName, Integer.toString(budget + Integer.parseInt(rows.get(i)[4]))));
-            }
-            else
+                map.replace(checkingAccount, new Pair(companyName, Integer.toString(ostatok)));
+                checkingAccount = rows.get(i)[3];
+                companyName = (String) map.get(checkingAccount).first;
+                budget = Integer.parseInt((String) map.get(checkingAccount).second);
+                map.replace(checkingAccount,
+                        new Pair(companyName, Integer.toString(budget + Integer.parseInt(rows.get(i)[4]))));
+            } else
                 log.info("Остаток средств на счете компании " + companyName + ": " + budget +
                         "\n Попытка отправить " + Integer.parseInt(rows.get(i)[4]) + " рублей" +
                         "компании " + map.get(rows.get(i)[3]).first);
         }
         ArrayList<String[]> result = new ArrayList<>();
-        for (HashMap.Entry<String, Pair> entry: map.entrySet())
+        for (HashMap.Entry<String, Pair> entry : map.entrySet())
             result.add(new String[]{entry.getValue().first + ";" + entry.getKey() + ";" + entry.getValue().second +
                     "\n"});
         File f = new File(dir + "\\" + "Result.csv");
--- CompanyesBuget/src/Pair.java
+++ CompanyesBuget/src/Pair.java
@@ -1,16 +1,17 @@
 public class Pair {
     Object first;
     Object second;
-    public Pair(Object first, Object second){
+
+    public Pair(Object first, Object second) {
         this.first = first;
         this.second = second;
     }
 
-    private Object getFirst(){
+    private Object getFirst() {
         return this.first;
     }
 
-    private Object getSecond(){
+    private Object getSecond() {
         return this.second;
     }
 }
